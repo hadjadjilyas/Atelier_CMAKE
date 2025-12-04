4a619439c65cbe4d065334d6bb1c7fbb1aa70155
hadjadjilyas <ilyashadjadj08@gmail.com>
2025-12-04 11:48:53 +0100

    Fix main.c braces and restore correct prints for operations

--- src/main.c
+++ src/main.c
@@ -11,19 +11,23 @@ int main(int argc, char *argv[]){
 		char* b = argv[3];
 		double r = 0;
 		if (strcmp(op, "add") == 0) {
-		if (strcmp(op, "add") == 0) {
 			r = _add(atof(a), atof(b));
+			printf("%lf", r);
 		}
 		else if (strcmp(op, "sub") == 0) {
 			r = _sub(atof(a), atof(b));
+			printf("%lf", r);
 		}
 		else if (strcmp(op, "mul") == 0) {
 			r = _mul(atof(a), atof(b));
+			printf("%lf", r);
 		}
 		else if (strcmp(op, "div") == 0) {
 			r = _div(atof(a), atof(b));
+			printf("%lf", r);
 		}
-		else {printf("Erreur de parametres");}
+		else { printf("Erreur de parametres"); }
+	}
 	else if (argc == 3)
 	{
 		char* op = argv[1];
@@ -32,13 +36,10 @@ int main(int argc, char *argv[]){
 		if (strcmp(op, "car") == 0) {
 			r = _car(atof(a));
 			printf("%lf", r);
-			printf("%lf", r);
 		}
-		else { 
-			printf("Erreur de parametres");
-		}
+		else { printf("Erreur de parametres"); }
 	}
-	else {printf("Erreur de parametres");}
+	else { printf("Erreur de parametres"); }
 	return 0;
 }
 
